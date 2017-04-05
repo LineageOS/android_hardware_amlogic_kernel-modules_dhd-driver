55869ca4f5645aec0f6399f7b5d66563d8655c13
Rongjun Chen <rongjun.chen@amlogic.com>
2017-04-05 10:16:51 +0800

    wifi: fix  bcmdhd.1.363.59.144.x.cn make fail issue
    Change-Id: I63db00421e4981ff3ef6ea6bf5c87aecc79b2fd4

--- bcmdhd.1.363.59.144.x.cn/linux_osl.c
+++ bcmdhd.1.363.59.144.x.cn/linux_osl.c
@@ -47,8 +47,9 @@
 #include <bcmutils.h>
 #include <linux/delay.h>
 #include <pcicfg.h>
-/*#include <asm-generic/pci-dma-compat.h>*/
-
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(4, 8, 0))
+#include <asm-generic/pci-dma-compat.h>
+#endif
 
 #ifdef BCM_SECURE_DMA
 #include <linux/module.h>
