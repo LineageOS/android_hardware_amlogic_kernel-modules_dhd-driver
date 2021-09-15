c846562cbd4a58d5256a745d9a9d36760e35941c
Jan Altensen <info@stricted.net>
2021-09-15 12:05:39 +0200

    [WIP] dhd: import wifi firmware
    todo:
     * mention where these come from
     * handle sabrina's firmware
    
    Change-Id: I0132b1f9bd92ed997f1a4654237f39ce150e23c4

---
+++ firmware/bluetooth/Android.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/4343.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/4343.hcd \
+    $(LOCAL_PATH)/BCM2076.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM2076.hcd \
+    $(LOCAL_PATH)/bcm4335c0.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bcm4335c0.hcd \
+    $(LOCAL_PATH)/BCM4354.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4354.hcd \
+    $(LOCAL_PATH)/bcm43241b4.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bcm43241b4.hcd \
+    $(LOCAL_PATH)/BCM4345C0.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4345C0.hcd \
+    $(LOCAL_PATH)/bcm43569a2.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bcm43569a2.hcd \
+    $(LOCAL_PATH)/BCM20702.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM20702.hcd \
+    $(LOCAL_PATH)/BCM4330.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4330.hcd \
+    $(LOCAL_PATH)/BCM4345C5.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4345C5.hcd \
+    $(LOCAL_PATH)/BCM4359C0.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4359C0.hcd \
+    $(LOCAL_PATH)/BCM20703A2.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM20703A2.hcd \
+    $(LOCAL_PATH)/bcm43341b0.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bcm43341b0.hcd \
+    $(LOCAL_PATH)/BCM4350.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4350.hcd \
+    $(LOCAL_PATH)/BCM4362A1.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/BCM4362A1.hcd \
