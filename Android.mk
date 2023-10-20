2ed3a03712ff271bb7a522f8a4b251f6a68aebe0
Bruno Martins <bgcngm@gmail.com>
2023-10-21 00:16:58 +0100

    dhd-driver: Strip modules using llvm-strip
    With this, the GCC dependency is gone for good.
    
    Change-Id: I61acca2e1fb91353154567b9bff340cf5ca99119

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ $(_dhd_ko): $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
 	@mkdir -p $(dir $@)
 	@cp -R $(DHD_PATH)/* $(dir $@)/
 	$(PATH_OVERRIDE) $(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C $(_dhd_intermediates) M=$(abspath $(_dhd_intermediates)) ARCH=$(TARGET_KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) $(KERNEL_CLANG_TRIPLE) $(KERNEL_CC) KERNEL_SRC=$(abspath $(KERNEL_OUT)) bcmdhd_sdio
-	$(KERNEL_TOOLCHAIN_PATH)strip --strip-unneeded $@;
+	$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-strip --strip-unneeded $@;
 
 include $(BUILD_SYSTEM)/base_rules.mk
 endif
