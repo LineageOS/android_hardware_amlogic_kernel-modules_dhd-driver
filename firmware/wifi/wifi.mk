544740b44398149ae838845fb34c48f869dcd527
Bruno Martins <bgcngm@gmail.com>
2024-07-07 18:26:36 +0100

    firmware: Update paths and delete main makefile
    Change-Id: I7dff5c25ca870c9441cd8eada46dcf51d60c5776

--- firmware/wifi/wifi.mk
+++ firmware/wifi/wifi.mk
@@ -1,20 +1,10 @@
 #
 # Copyright (C) 2021-2024 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-WIFI_FIRMWARE_PATH := hardware/amlogic/kernel-modules/dhd-driver/firmware/wifi
+WIFI_FIRMWARE_PATH := kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi
 
 # bcm4339a0_ag ap6335
 PRODUCT_COPY_FILES += \
